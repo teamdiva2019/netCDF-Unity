add3a33c6a9bf41c96b84e27b8a4fcce732e9e83
Naeem Alam <naeemtalam@gmail.com>
2016-12-28 17:59:02 -0500

    Reads data from data.csv and plots data in space.

---
+++ Interactive Visualization/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	// go to initial position
+	void Start () {
+        transform.position = new Vector3(0, 0, -30);
+	}
+	
+	// camera
+	void Update () {
+        transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime); // RotateAround(point, axis, angle in deg)
+        transform.LookAt(new Vector3(0, 0, 0));
+	}
+}
--- Interactive Visualization/Assets/Scripts/SphereScript.cs
+++ Interactive Visualization/Assets/Scripts/SphereScript.cs
@@ -1,33 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.IO;
+using UnityEngine.UI;
+using System.Reflection;
 
 public class SphereScript : MonoBehaviour {
 
-    private float phi = 0;
-    private float rho = 1;
-    private float theta = 0;
-    private ArrayList s = new ArrayList(); // contains all the spheres representing data points.
-	
-	// once per frame, move a tiny bit around unit sphere
-	void Update () {
-        phi += Mathf.PI / 180;
+    private ArrayList lat = new ArrayList(); // latitudes
+    private ArrayList lon = new ArrayList(); // longitudes
+    private ArrayList alt = new ArrayList(); // altitudes
+    private ArrayList data = new ArrayList(); // data pts
 
-        float r = rho * Mathf.Sin(phi);
-        float x = r * Mathf.Cos(theta);
-        float y = r * Mathf.Sin(theta);
-        float z = rho * Mathf.Cos(phi);
+    public Text cnsl; // this is the console. If there are errors, I will write them here.
 
-        if (phi <= 2 * Mathf.PI)
-        { // if phi is less than 2pi
+    private int DATA_MAX = 10; // our data points can have values from 0 to 10.
+
+    private ArrayList pts = new ArrayList(); // contains the actual spheres in 3-space
+
+    // reads in all the data to start with and stores it in the ArrayLists
+    void Start ()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        transform.localScale = new Vector3(1, 1, 1);
+        cnsl.text = "Test text."; // clear console
+        cnsl.transform.position = new Vector3(0, 0, 0);
+        ReadData();
+        GeneratePts();
+    } // end function Start
+
+    // creates the data points
+    void GeneratePts () {
+        for (int i = 0; i < lat.Count; i++)
+        {
+
+            float rho, theta, phi;
+            rho = (float)alt[i];
+            theta = (float)lon[i] * 180/Mathf.PI; // note the conversion to rad
+            phi = (90 - (float)lat[i]) * 180/Mathf.PI; // note the conversion to rad
+
+            float r = rho * Mathf.Sin(phi);
+            float x = r * Mathf.Cos(theta);
+            float y = r * Mathf.Sin(theta);
+            float z = rho * Mathf.Cos(phi);
+
             GameObject tmp = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             tmp.transform.position = new Vector3(x, y, z);
-            tmp.transform.localScale = new Vector3(0.1F, 0.1F, 0.1F);
+            // tmp.transform.localScale = new Vector3(0.1F, 0.1F, 0.1F); // note that the Fs indicate floats; without them, it's a double.
             Renderer rd = (Renderer)tmp.GetComponent("Renderer");
-            rd.material.color = new Color(Random.value, Random.value, Random.value, Random.value);
-            s.Add(tmp);            
-        } // end if
-    } // end function Update()
+            rd.material.color = new Color((float)data[i]/DATA_MAX, 1 - (float)data[i] / DATA_MAX, 0, 0.5F); // green for low levels, red for high.
+            pts.Add(tmp);
+
+        } // end for
+    } // end function GeneratePts()
 
     // this makes the script delay for d seconds. Note that function names are capitalized.
     IEnumerator Wait(float d)
@@ -36,13 +60,15 @@ public class SphereScript : MonoBehaviour {
     } // end function Wait()
 
     // this function reads the file data.txt to populate Arraylists for lat, lon, alt, and data.
-    void ReadData(ArrayList lat, ArrayList lon, ArrayList alt, ArrayList data)
+    void ReadData()
     {
         try
         {
             // Create an instance of StreamReader to read from a file.
             // The using statement also closes the StreamReader.
-            using (StreamReader sr = new StreamReader("..\\..\\data.csv"))
+            // Note that location of executing assemebly is: *\netCDF-Unity\Interactive Visualization\Library\ScriptAssemblies\Assembly-CSharp.dll
+            // Note also that the .dll is a folder containing the assembly, so that's why we need to go three levels up from it.
+            using (StreamReader sr = new StreamReader(Assembly.GetExecutingAssembly().Location+"\\..\\..\\..\\Assets\\Scripts\\data.csv"))
             {
 
                 string line;
@@ -74,8 +100,7 @@ public class SphereScript : MonoBehaviour {
         catch (System.Exception e)
         {
             // Let the user know what went wrong.
-            System.Console.WriteLine("The file could not be read:");
-            System.Console.WriteLine(e.Message);
+            cnsl.text = "File not found.\nLocation of Executing Assembly (for reference): "+ Assembly.GetExecutingAssembly().Location;
         } // end catch
 
     } // end function ReadData
