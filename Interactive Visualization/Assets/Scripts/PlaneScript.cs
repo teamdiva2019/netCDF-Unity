a0dd8f496f55ef8958dc9494a6200bff812af808
Naeem Alam <naeemtalam@gmail.com>
2016-12-29 16:30:09 -0500

    Relative paths added to PlaneScript.cs, but still some errors.

--- Interactive Visualization/Assets/Scripts/PlaneScript.cs
+++ Interactive Visualization/Assets/Scripts/PlaneScript.cs
@@ -3,29 +3,39 @@ using System;
 using System.IO;
 using System.Diagnostics;
 using System.Collections;
+using UnityEngine.UI;
+using System.Reflection;
+
 public class PlaneScript : MonoBehaviour {
 
 	private Texture2D myTexture;
 	private Material myMaterial;
+    public Text assemblyLocation; //Assembly Location: "*\netCDF-Unity\Interactive Visualization\Library\ScriptAssemblies\Assembly-CSharp.dll". 3 ..'s get you to Interactive Visualization.
+                                  // we need to know the location of the assembly to use relative paths.
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
 		GameObject plane = GameObject.CreatePrimitive (PrimitiveType.Plane);
 
-		/*taken from http://stackoverflow.com/questions/20764049/how-to-execute-shell-script-in-c-sharp*/
-		Process proc = new Process {
+        // this is so we can use relative paths.
+        assemblyLocation.text = "Assembly Location: "+Assembly.GetExecutingAssembly().Location; // lets you see it.
+        string loc = Assembly.GetExecutingAssembly().Location;
+
+
+        /*taken from http://stackoverflow.com/questions/20764049/how-to-execute-shell-script-in-c-sharp*/
+        Process proc = new Process {
 			StartInfo = new ProcessStartInfo {
 				FileName = "python.exe",
-				Arguments = "\"..\\ReadnCDF\\PlotData.py\"", // Change this to the full path
+				Arguments = loc+"\\..\\..\\..\\Assets\\ReadnCDF\\PlotData.py", // Change this to the full path
 				UseShellExecute = false,
 				RedirectStandardOutput = true,
 				CreateNoWindow = true
 			}
 		};
 		// Delete the time.txt if it's there
-		string imageString = "\"..\\Resources\\Plot.png\"";
-		// File.Delete("\"..\\ReadnCDF\\time.txt\""); // what happens if I take this line out?
+		string imageString = loc + "\\..\\..\\..\\Assets\\Resources\\Plot.png";
+		File.Delete(loc + "\\..\\..\\..\\Assets\\ReadnCDF\\time.txt");
 		proc.Start ();
 		proc.Close ();
 
